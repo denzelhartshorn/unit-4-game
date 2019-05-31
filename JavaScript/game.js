1ccdd35fdd48230142196b1b369a0d83f6a0a66c
Denzel <denzelhartshorn@gmail.com>
2019-05-30 19:48:05 -0700

    rough java & bootstrap

---
+++ JavaScript/game.js
@@ -0,0 +1,54 @@
+
+
+
+
+var fighterList = ["Jon", "Jamie", "Daenerys", "Arya"];
+    console.log(fighters)
+
+var fighterList = $("#fighter");
+
+    var Jon = {
+        Health: ""
+        Attack: ""
+        Counter Attack: ""
+    }
+
+    var Jamie = {
+        Health: ""
+        Attack: ""
+        Counter Attack: ""
+    }
+
+    var Daenerys = {
+        Health: ""
+        Attack: ""
+        Counter Attack: ""
+    }
+
+    var Arya = {
+        Health: ""
+        Attack: ""
+        Counter Attack: ""
+    }
+
+    // move fighter to fight area function
+    $("#yourfighter").on("click", function() {
+        alert("move fighter to area")
+    }
+
+
+// fight function
+    fightbutton: function() {
+        alert("figure out the fight function");
+    }
+
+// fight button
+$("#fightbutton").on("click", function() {
+    alert("make the fight button work!");
+  })
+  console.log(fightbutton)
+
+// < !--Link to song-- >
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "https://youtu.be/s7L2PVdrb_8");
+    console.log(audioElement)
